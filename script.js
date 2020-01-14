993ae4b1d980457e94ebfe7231ccfcb310071e7d
Yash Raj Bharti <43868318+yashrajbharti@users.noreply.github.com>
2020-01-14 15:21:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var Swipes = new Swiper('.swiper-container', {
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+});
